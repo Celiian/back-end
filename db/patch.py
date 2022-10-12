e717b0f8bd88a134b37163cd329e13d8a63a5ec8
Héloïse <lelez.heloise@gmail.com>
2022-10-13 00:27:43 +0200

    Feature/ patch employees and comments

--- db/patch.py
+++ db/patch.py
@@ -1,4 +1,5 @@
 from db.db_params import *
+from db.get import get_team
 
 
 def patch_enclosures(id, cost, biome):
@@ -23,4 +24,47 @@ def patch_enclosures(id, cost, biome):
         query += " biome = %s"
     record.append(id)
     query += " WHERE id_enclosure = %s"
+    return update_data(query, record)
+
+
+def patch_employees(id, id_team, family_name, phone_number, emergency_contact):
+    """
+    Edit employees informations
+
+    :param id: INT REQUUIRED
+    :param id_team: INT OPTIONAL
+    :param family_name: STRING OPTIONAL
+    :param phone_number: STRING OPTIONAL
+    :param emergency_contact: STRING OPTIONAL
+    :return: BOOLEAN Table updated or not
+    """
+    query = "UPDATE employees SET"
+    record = []
+    preceded = False
+    if get_team(id_team):
+        record.append(id_team)
+        query += " id_team = %s"
+        preceded = True
+
+    if family_name:
+        if preceded:
+            query += ","
+        record.append(family_name)
+        query += " family_name = %s"
+        preceded = True
+
+    if phone_number:
+        if preceded:
+            query += ","
+        record.append(phone_number)
+        query += " phone_number = %s"
+        preceded = True
+
+    if emergency_contact:
+        if preceded:
+            query += ","
+        record.append(emergency_contact)
+        query += " emergency_contact = %s"
+    record.append(id)
+    query += "WHERE id_employee_member = %s"
     return update_data(query, record)
