579a6e9f6ce817d4d08c44b5211cdda4021e4ad0
admin <admin@MacBook-Pro-de-felix.local>
2022-10-12 15:09:45 +0200

    FEATURE/Added post function for enclosure + fix password.py

--- sql_fill/sql_table_fill.sql
+++ sql_fill/sql_table_fill.sql
@@ -19,76 +19,76 @@ VALUES  ('Triceratops','40','vegetation','late cretaceous','jungle',10000),
 
 
 
-INSERT INTO enclosures (id_enclosure, biome, maintenance_cost )
-VALUES  (0,'jungle', 95000),
-        (1,'forest', 17000),
-        (2,'forest', 6),
-        (3,'lowland', 72000),
-        (4,'forest', 25000),
-        (5,'aquatic', 400);
+INSERT INTO enclosures (biome, maintenance_cost )
+VALUES  ('jungle', 95000),
+        ('forest', 17000),
+        ('forest', 6),
+        ('lowland', 72000),
+        ('forest', 25000),
+        ('aquatic', 400);
 
 
 
 
 
 
-INSERT INTO teams(id_team , team_type, vehicle_type )
-VALUES      (0,'Intervention','helicopter'),
-           (1,'Intervention','car'),
-           (2,'Intervention','car'),
-           (3,'Medical','helicopter'),
-           (4,'Medical','car'),
-           (5,'Medical','car'),
-           (6,'restock','car'),
-           (7,'restock','car'),
-           (8,'restock','car'),
-           (9,'Cleaning','car'),
-           (10,'Cleaning','car'),
-           (11,'Cleaning','car');
+INSERT INTO teams(team_type, vehicle_type)
+VALUES      ('Intervention','helicopter'),
+           ('Intervention','car'),
+           ('Intervention','car'),
+           ('Medical','helicopter'),
+           ('Medical','car'),
+           ('Medical','car'),
+           ('restock','car'),
+           ('restock','car'),
+           ('restock','car'),
+           ('Cleaning','car'),
+           ('Cleaning','car'),
+           ('Cleaning','car');
 
 
-INSERT INTO employees(id_employee_member, id_team, family_name, surname, phone_number, social_security_member, emergency_contact )
-VALUES      (0,0 ,'Dupon', 'Héloïse', '0738293819', '0313834719381','0738293819'),
-            (1,0 ,'Dupont', 'Léo', '0738293819', '0313834719381','0738293819'),
-            (2,0 ,'Dupond', 'Célian', '0738293819', '031383471938', '0738293819' ),
+INSERT INTO employees(id_team, family_name, surname, phone_number, social_security_member, emergency_contact )
+VALUES      (12 ,'Dupon', 'Héloïse', '0738293819', '0313834719381','0738293819'),
+            (12 ,'Dupont', 'Léo', '0738293819', '0313834719381','0738293819'),
+            (12 ,'Dupond', 'Célian', '0738293819', '031383471938', '0738293819' ),
 
-            (3,1 ,'Dupons', 'Mariette', '0738233819', '0313834719381', '0738293819'),
-            (4,1 ,'Duponts', 'Bernadette', '0738293819', '031383471938', '0738293819' ),
-             (5,1 ,'Duponds', 'Isidor', '0738293819', '031383471938', '0738293819' ),
+            (1 ,'Dupons', 'Mariette', '0738233819', '0313834719381', '0738293819'),
+            (1 ,'Duponts', 'Bernadette', '0738293819', '031383471938', '0738293819' ),
+             (1 ,'Duponds', 'Isidor', '0738293819', '031383471938', '0738293819' ),
 
-            (6,2 ,'Duppon', 'René', '0738293819', '031383471938', '0738293819' ),
-            (7,2 ,'Dupport', 'Claudette', '0738293819', '031383471938', '0738293819' ),
-            (8,2 ,'Duppond', 'Lisette', '0738293819', '031383471938', '0738293819' ),
+            (2 ,'Duppon', 'René', '0738293819', '031383471938', '0738293819' ),
+            (2 ,'Dupport', 'Claudette', '0738293819', '031383471938', '0738293819' ),
+            (2 ,'Duppond', 'Lisette', '0738293819', '031383471938', '0738293819' ),
 
-            (9,3 ,'Duppons', 'Patrick', '0738293819', '031383471938', '0738293819' ),
-            (10,3 ,'Dupponts', 'Patricia', '0738293819', '031383471938', '0738293819' ),
-            (11,3 ,'Dupponds', 'Frank', '0738293819', '031383471938', '0738293819' ),
+            (3 ,'Duppons', 'Patrick', '0738293819', '031383471938', '0738293819' ),
+            (3 ,'Dupponts', 'Patricia', '0738293819', '031383471938', '0738293819' ),
+            (3 ,'Dupponds', 'Frank', '0738293819', '031383471938', '0738293819' ),
 
-            (12,4 ,'Duipon', 'Edgard', '0738293819', '031383471938', '0738293819' ),
-            (13,4 ,'Duipont', 'Lilianne', '0738293819', '031383471938', '0738293819' ),
-            (14,4 ,'Dupond', 'Josselin', '0738293819', '031383471938', '0738293819' ),
+            (4 ,'Duipon', 'Edgard', '0738293819', '031383471938', '0738293819' ),
+            (4 ,'Duipont', 'Lilianne', '0738293819', '031383471938', '0738293819' ),
+            (4 ,'Dupond', 'Josselin', '0738293819', '031383471938', '0738293819' ),
 
-            (15,5 ,'Duphond', 'Jean-François-Ferdinant le 3', '0738293819', '031383471938', '0738293819' ),
-            (16,5 ,'Duphont', 'Marie-Huguette-Henriette la 2', '0738293819', '031383471938', '0738293819' ),
-            (17,5 ,'Duhphon', 'Viviane-Justine-Viviane', '0738293819', '0313834719381', '0738293819' ),
+            (5 ,'Duphond', 'Jean-François-Ferdinant le 3', '0738293819', '031383471938', '0738293819' ),
+            (5 ,'Duphont', 'Marie-Huguette-Henriette la 2', '0738293819', '031383471938', '0738293819' ),
+            (5 ,'Duhphon', 'Viviane-Justine-Viviane', '0738293819', '0313834719381', '0738293819' ),
 
-            (18,6 ,'Duplomb', 'Harnold', '0738293819', '031383471938', '0738293819' ),
-            (19,6 ,'DuSansPlomb95', 'Gasparde', '0738293819', '031383471938', '0738293819' ),
+            (6 ,'Duplomb', 'Harnold', '0738293819', '031383471938', '0738293819' ),
+            (6 ,'DuSansPlomb95', 'Gasparde', '0738293819', '031383471938', '0738293819' ),
 
-            (20,7 ,'Dupondpond', 'Hernest', '0738293819', '031383471938', '0738293819' ),
-            (21,7 ,'Dupontpont', 'Marceline', '0738293819', '031383471938', '0738293819' ),
+            (7 ,'Dupondpond', 'Hernest', '0738293819', '031383471938', '0738293819' ),
+            (7 ,'Dupontpont', 'Marceline', '0738293819', '031383471938', '0738293819' ),
 
-            (22,8 ,'Pondu', 'Martine', '0738293819', '031383471938', '0738293819' ),
-            (23,8 ,'tonpu', 'Gaston et Octave et Oscar et Gustave', '0738293819', '031383471938', '0738293819' ),
+            (8 ,'Pondu', 'Martine', '0738293819', '031383471938', '0738293819' ),
+            (8 ,'tonpu', 'Gaston et Octave et Oscar et Gustave', '0738293819', '031383471938', '0738293819' ),
 
-            (24,9 ,'DeLaPasserelle', 'Jean-François-Antoine', '0738293819', '031383471938', '0738293819' ),
-            (25,9 ,'d-d-d-d-duuu-dudu-du-p-p-p-p-ond', 'Didier', '0738293819', '031383471938', '0738293819' ),
+            (9 ,'DeLaPasserelle', 'Jean-François-Antoine', '0738293819', '031383471938', '0738293819' ),
+            (9 ,'d-d-d-d-duuu-dudu-du-p-p-p-p-ond', 'Didier', '0738293819', '031383471938', '0738293819' ),
 
-            (26,10 ,'Dupong', 'Marcel', '0738293819', '031383471938', '0738293819' ),
-            (27,10 ,'Duping', 'Ernestine', '0738293819', '031383471938', '0738293819' ),
+            (10 ,'Dupong', 'Marcel', '0738293819', '031383471938', '0738293819' ),
+            (10 ,'Duping', 'Ernestine', '0738293819', '031383471938', '0738293819' ),
 
-            (28,11 ,'tiponche', 'Yvon', '0738293819', '031383471938', '0738293819' ),
-            (29,11 ,'DuPompe', 'Yvonne', '0738293819', '031383471938', '0738293819' );
+            (11 ,'tiponche', 'Yvon', '0738293819', '031383471938', '0738293819' ),
+            (11 ,'DuPompe', 'Yvonne', '0738293819', '031383471938', '0738293819' );
 
 
 INSERT INTO teams_organisations (id_enclosure, id_team )
@@ -122,17 +122,17 @@ VALUES  (0,1),
 INSERT INTO dinosaurs(dinosaur_name, breed_name, id_enclosure, creation_date, gender, height, weigh, id_employees)
 VALUES  ('Héloïse','Compsognathus',2,'2002-01-06','female',1,3,2),
         ('Léo','Allausore',1,'2003-12-04','male',4,5183,1),
-        ('Célian','Tyrannosaure',0,'2002-10-24','male',5,8238,2),
+        ('Célian','Tyrannosaure',6,'2002-10-24','male',5,8238,2),
         ('Anthony','Diplodocus',3,'1979-07-1','female',29,15203,1),
         ('Thiery','Nyasasaurus',3,'1850-02-01','male',3,50,2),
         ('Kevin','Thyreophora',3,'2002-10-08','female',8,40283,1),
         ('Christopher','Steakosaure',3,'2001-12-12','male',9,3271,1),
         ('Lucas','Thyreophora',4,'2003-05-23','female',7,60193,1),
-        ('David','Steakosaure',4,'2001-03-30','male',7,3928,0),
-        ('Maxime','Diplodocus',4,'2004-10-03','female',3,13291,0),
+        ('David','Steakosaure',4,'2001-03-30','male',7,3928,3),
+        ('Maxime','Diplodocus',4,'2004-10-03','female',3,13291,3),
         ('Kevine','Thyreophora',4,'2003-09-08','male',9,80201,2),
-        ('Jérance','Thyreophora',3,'2007-10-13','female',10,85392,0),
-        ('Mathilde','Compsognathus',2,'2006-11-28','female',1,2,0),
+        ('Jérance','Thyreophora',3,'2007-10-13','female',10,85392,3),
+        ('Mathilde','Compsognathus',2,'2006-11-28','female',1,2,3),
         ('Julien','Nyasasaurus',4,'2004-08-19','female',2,30,2),
         ('Philémon','Thyreophora',3,'2005-12-10','male',4,620294,1),
         ('Mohamed','Steakosaure',4,'2005-10-04','female',8,3912,1),
