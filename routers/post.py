ed47e40a795c8ef72d510e151fc6bb267659145c
admin <admin@MacBook-Pro-de-felix.local>
2022-10-12 15:11:35 +0200

    FEATURE/Post enclosure complete

--- routers/post.py
+++ routers/post.py
@@ -6,6 +6,13 @@ router = APIRouter()
 
 @router.post("/enclosures")
 def post_enclosure(biome: str, maintenance_cost: str):
+    """
+    Calls postEnclosure function using url parameters as parameter, then returns whether post fails or succeeds
+
+    :param biome: The biome string of the enclosure, in the existing biomes
+    :param maintenance_cost: The daily cost of maintenance of the enclosure, as a string
+    :return: JSON The response with a status code and a message
+    """
     data = (biome, maintenance_cost)
     res = postEnclosure(data)
     if res:
