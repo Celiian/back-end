ba2f12c126554cc828f5b9a83b3763fe30d0486b
Héloïse <lelez.heloise@gmail.com>
2022-10-12 16:37:30 +0200

    FIX/ added selectData function

--- db/db_params.py
+++ db/db_params.py
@@ -1,9 +1,9 @@
 import mysql.connector as mc
 from password import returnPassword
+password = returnPassword()
 
 
 def update_data(query, record):
-    password = returnPassword()
     db = mc.connect(user='root',
                     password=password,
                     host='127.0.0.1',
@@ -16,3 +16,24 @@ def update_data(query, record):
         return True
     except mc.Error:
         return False
+
+def selectData(query, record=None):
+    """
+    connect to the database and perform a query
+
+    :param query:STRING REQUIRED the query to be made
+    :param record: STRING OPTIONAL
+    :return: LIST all the data from the query
+    """
+
+    db = mc.connect(user='root',
+                    password=password,
+                    host='127.0.0.1',
+                    database='jurassic')
+
+    my_cursor = db.cursor()
+    result = []
+    my_cursor.execute(query, record)
+    for i in my_cursor:
+        result.append(i)
+    return result
