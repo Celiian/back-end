d9d426a3772f712565a4b35a6d98fd57976c26c3
Héloïse <lelez.heloise@gmail.com>
2022-10-12 23:24:06 +0200

    Feature/ dinosaur patch and comments

--- db/patch.py
+++ db/patch.py
@@ -1,4 +1,5 @@
 from db.db_params import *
+from db.get import get_enclosure, get_employee
 
 
 def patch_enclosures(id, cost, biome):
@@ -23,4 +24,58 @@ def patch_enclosures(id, cost, biome):
         query += " biome = %s"
     record.append(id)
     query += " WHERE id_enclosure = %s"
+    return update_data(query, record)
+
+
+def patch_dinosaurs(dinosaur_name, id_enclosure, gender, height, weight, id_employees):
+    """
+    Edit dinosaur's informations
+
+    :param dinosaur_name: STRING REQUIRED dinosaur name
+    :param id_enclosure: INT OPTIONAL dinosaur enclosure id
+    :param gender: STRING OPTIONAL dinosaur gender
+    :param height: INT OPTIONAL dinosaur height
+    :param weight: INT OPTIONAL dinosaur weight
+    :param id_employees: INT OPTIONAL dinosaur employee id
+    :return: BOOLEAN Table updated or not
+    """
+    query = "UPDATE dinosaurs SET"
+    record = []
+    preceded = False
+
+    if gender:
+        record.append(gender)
+        query += " gender = %s"
+        preceded = True
+
+    if get_enclosure(id_enclosure):
+
+        if preceded:
+            query += ","
+        record.append(id_enclosure)
+        query += " id_enclosure = %s"
+        preceded = True
+
+    if height:
+        if preceded:
+            query += ","
+        record.append(height)
+        query += " height = %s"
+        preceded = True
+
+    if weight:
+        if preceded:
+            query += ","
+        record.append(weight)
+        query += " weight = %s"
+        preceded = True
+
+    if get_employee(id_employees):
+        if preceded:
+            query += ","
+        record.append(id_employees)
+        query += " id_employees = %s"
+    record.append(dinosaur_name)
+    query += " WHERE dinosaur_name = %s"
+    print(query)
     return update_data(query, record)
--- routers/patch.py
+++ routers/patch.py
@@ -1,8 +1,10 @@
+import datetime
+
 from fastapi import APIRouter
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 
-from db.patch import patch_enclosures
+from db.patch import *
 
 router = APIRouter()
 
@@ -26,6 +28,46 @@ async def enclosure(id: int, body: Item):
     print(body)
 
     res = patch_enclosures(id, cost, biome)
+
+    if res:
+        return JSONResponse(
+            status_code=200,
+            content={"Message": "Table updated"}
+        )
+    else:
+        return JSONResponse(
+            status_code=404,
+            content={"Message": "Table not updated"}
+        )
+
+
+class Dino(BaseModel):
+    id_enclosure: int | None
+    gender: str | None
+    height: int | None
+    weight: int | None
+    id_employees: int | None
+
+
+@router.patch("/dinosaurs/{name}")
+def dinosaurs(name: str, body: Dino):
+    """
+    Edit a dinosaur informations
+
+    :param name: STRING REQUIRED dinosaur name
+    :param body: LIST parameters and values for patch_dinosaurs
+    :return: Message Table updated or not
+    """
+    id_enclosure = body.id_enclosure
+    gender = body.gender
+    height = body.height
+    weight = body.weight
+    id_employees = body.id_employees
+
+    print(name)
+    print(body)
+
+    res = patch_dinosaurs(name, id_enclosure, gender, height, weight, id_employees)
 
     if res:
         return JSONResponse(
