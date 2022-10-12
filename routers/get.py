00770642936bfa49939c0012c53608c0154510c5
Célian <celian@MacBook-Pro-de-admin-5.local>
2022-10-12 14:28:35 +0200

    FEATURE/ teams request done, commented and clean

--- routers/get.py
+++ routers/get.py
@@ -18,6 +18,7 @@ class Message(BaseModel):
 async def enclosures():
     """
     Send all enclosures
+
     :return: JSON A status code and the data
     """
     data = get_enclosures()
@@ -36,6 +37,7 @@ async def enclosures():
 async def enclosure(id: int):
     """
     Send a specific enclosure
+
     :param id: INT REQUIRED The id of the enclosure
     :return: JSON A status code and the data
     """
@@ -61,6 +63,7 @@ async def enclosure(id: int):
 async def enclosure_dinosaurs(id: int):
     """
     Send all dinosaurs living in an enclosure
+
     :param id: INT REQUIRED The id of the enclosure
     :return: JSON A status code and the data
     """
@@ -86,6 +89,7 @@ async def enclosure_dinosaurs(id: int):
 async def enclosure_teams(id: int):
     """
     Send all teams working in an enclosure
+
     :param id: INT REQUIRED The id of the enclosure
     :return: JSON A status code and the data
     """
@@ -111,10 +115,108 @@ async def enclosure_teams(id: int):
 async def enclosure_teams_employees(id: int):
     """
     Send all employees working in an enclosure
+
     :param id: INT REQUIRED The id of the enclosure
     :return: JSON A status code and the data
     """
     data = get_enclosure_teams_employees(id)
+    if data:
+        return JSONResponse(
+            status_code=200,
+            content=data
+        )
+    else:
+        return JSONResponse(
+            status_code=404,
+            content={"Message": "this id does not exist"}
+        )
+
+
+@router.get("/teams",
+            status_code=200,
+            description="Get all the teams",
+            responses={
+                404: {"model": Message}
+            })
+async def teams():
+    """
+    Send all teams
+
+    :return:JSON A status code and the data
+    """
+    data = get_teams()
+    return JSONResponse(
+        status_code=200,
+        content=data
+    )
+
+
+@router.get("/teams/{id}",
+            status_code=200,
+            description="Get a specific team",
+            responses={
+                404: {"model": Message}
+            })
+async def team(id: int):
+    """
+    Send a specific team
+
+    :param id: INT REQUIRED The id of the enclosure
+    :return: JSON A status code and the data
+    """
+    data = get_team(id)
+    if data:
+        return JSONResponse(
+            status_code=200,
+            content=data
+        )
+    else:
+        return JSONResponse(
+            status_code=404,
+            content={"Message": "this id does not exist"}
+        )
+
+
+@router.get("/teams/{id}/employees",
+            status_code=200,
+            description="Get all employees of a team",
+            responses={
+                404: {"model": Message}
+            })
+async def team_employees(id: int):
+    """
+    Send all employees of a team
+
+    :param id: INT REQUIRED The id of the enclosure
+    :return: JSON A status code and the data
+    """
+    data = get_team_employees(id)
+    if data:
+        return JSONResponse(
+            status_code=200,
+            content=data
+        )
+    else:
+        return JSONResponse(
+            status_code=404,
+            content={"Message": "this id does not exist"}
+        )
+
+
+@router.get("/teams/{id}/enclosures",
+            status_code=200,
+            description="Get all enclosures managed by a team",
+            responses={
+                404: {"model": Message}
+            })
+async def team_enclosures(id: int):
+    """
+    Send a all enclosures managed by a team
+
+    :param id: INT REQUIRED The id of the enclosure
+    :return: JSON A status code and the data
+    """
+    data = get_team_enclosures(id)
     if data:
         return JSONResponse(
             status_code=200,
