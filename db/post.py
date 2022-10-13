b01acff8d0ccb6bdc38dc6b014710c68799b56cf
admin <admin@MacBook-Pro-de-felix.local>
2022-10-13 14:24:19 +0200

    FEATURE/ More consistent docstring

--- db/post.py
+++ db/post.py
@@ -31,9 +31,9 @@ def postTeam(data):
 
 def postFood(data):
     """
-    Defines the query string, then calls insertData function using data and post_breed_query as parameter
+    Defines the query string, then calls insertData function using data and post_food_query as parameter
 
-    :param data: OBJECT Containing all the values to insert in the query
+    :param data: OBJECT REQUIRED Containing all the values to insert in the query
     :return: BOOL The boolean value that becomes true if post succeeds or false if post fails.
     """
     post_food_query = "INSERT INTO food_supplies(food_type, price) VALUES (%s,%s)"
@@ -46,7 +46,7 @@ def postBreed(data):
     """
     Defines the query string, then calls insertData function using data and post_breed_query as parameter
 
-    :param data: OBJECT Containing all the values to insert in the query
+    :param data: OBJECT REQUIRED Containing all the values to insert in the query
     :return: BOOL The boolean value that becomes true if post succeeds or false if post fails.
     """
     post_breed_query = "INSERT INTO breeds(breed_name, food_eaten_daily, regime_type, era, biome_needed, price) VALUES (%s,%s,%s,%s,%s,%s)"
@@ -59,7 +59,7 @@ def postDinosaur(data):
     """
     Defines the query string, then calls insertData function using data and post_dinosaur_query as parameter
 
-    :param data: OBJECT Containing all the values to insert in the query
+    :param data: OBJECT REQUIRED Containing all the values to insert in the query
     :return: BOOL The boolean value that becomes true if post succeeds or false if post fails.
     """
     post_dinosaur_query = "INSERT INTO dinosaurs(dinosaur_name, breed_name, id_enclosure, creation_date, gender, height, weigh, id_employees) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
@@ -71,7 +71,7 @@ def postEnclosure(data):
     """
     Defines the query string, then calls insertData function using data and post_enclosure_query as parameter
 
-    :param data: JSON The tuple containing the values of biome and maintenance_cost
+    :param data: OBJECT REQUIRED The tuple containing the values of biome and maintenance_cost
     :return: BOOL The boolean value that becomes true if post succeeds or false if post fails.
     """
     post_enclosure_query = "INSERT INTO enclosures(biome,maintenance_cost) VALUES (%s,%s)"
@@ -83,8 +83,8 @@ def postEmployee(data):
     """
     Defines the query string, then calls insertData function using data and post_employee_query as parameter
 
-    :param data: TUPLE REQUIRED The tuple containing the values of id_team, family_name,surname,phone_number,social_security_member,emergency_contact
-    :return: The boolean value that becomes true if post succeeds or false if post fails.
+    :param data: OBJECT REQUIRED The tuple containing the values of id_team, family_name,surname,phone_number,social_security_member,emergency_contact
+    :return: BOOL The boolean value that becomes true if post succeeds or false if post fails.
     """
     post_employee_query = "INSERT INTO employees(id_team,family_name,surname,phone_number,social_security_member,emergency_contact) VALUES (%s,%s,%s,%s,%s,%s)"
     res = insertData(post_employee_query, data)
