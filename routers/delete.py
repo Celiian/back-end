31dfb11c93fe551bc5a35282a357f406c248a109
Célian <celian@MacBook-Pro-de-admin-6.local>
2022-10-14 09:46:59 +0200

    FIX/ fixed comments in routers/delete.py

--- routers/delete.py
+++ routers/delete.py
@@ -21,14 +21,14 @@ class Message(BaseModel):
                responses={
                    400: {"model": Message}
                })
-async def enclosures(id: int):
+async def enclosures(enclosure_id: int):
     """
     Send all enclosures
 
-    :param id: INT REQUIRED Id of the enclosure
+    :param enclosure_id: INT REQUIRED Id of the enclosure
     :return: JSON A status code and a message
     """
-    res = delete_enclosure(id)
+    res = delete_enclosure(enclosure_id)
 
     return JSONResponse(
         status_code=200,
@@ -42,14 +42,14 @@ async def enclosures(id: int):
                responses={
                    400: {"model": Message}
                })
-async def teams(id: int):
+async def teams(team_id: int):
     """
     Send all enclosures
 
-    :param id: INT REQUIRED Id of the team
+    :param team_id: INT REQUIRED Id of the team
     :return: JSON A status code and a message
     """
-    res = delete_team(id)
+    res = delete_team(team_id)
     return JSONResponse(
         status_code=200,
         content={"Message": res["message"]}
@@ -84,14 +84,14 @@ async def food_supply(food_name: str):
                    400: {"model": Message}
                }
                )
-async def employees(id: int):
+async def employees(employee_id: int):
     """
     Delete a food supply
 
-    :param id: INT REQUIRED Id of the employee
+    :param employee_id: INT REQUIRED Id of the employee
     :return: JSON A status code and a message
     """
-    res = delete_employees(id)
+    res = delete_employees(employee_id)
     return JSONResponse(
         status_code=200,
         content={"Message": res["message"]}
@@ -105,14 +105,14 @@ async def employees(id: int):
                    400: {"model": Message}
                }
                )
-async def dinosaurs(name: str):
+async def dinosaurs(dinosaur_name: str):
     """
     Delete a dinosaur
 
-    :param name: STR REQUIRED Name of the dinosaur
+    :param dinosaur_name: STR REQUIRED Name of the dinosaur
     :return: JSON A status code and a message
     """
-    res = delete_dinosaurs(name)
+    res = delete_dinosaurs(dinosaur_name)
     return JSONResponse(
         status_code=200,
         content={"Message": res["message"]}
@@ -126,21 +126,21 @@ async def dinosaurs(name: str):
                    400: {"model": Message}
                }
                )
-async def breeds(name: str):
+async def breeds(breed_name: str):
     """
     Delete a breeds
 
-    :param name: STR REQUIRED Name of the breed
+    :param breed_name: STR REQUIRED Name of the breed
     :return: JSON A status code and a message
     """
-    res = delete_breeds(name)
+    res = delete_breeds(breed_name)
     return JSONResponse(
         status_code=200,
         content={"Message": res["message"]}
     )
 
 
-class item(BaseModel):
+class teams_organisation_ids(BaseModel):
     id_team: int
     id_enclosure: int
 
@@ -152,7 +152,7 @@ class item(BaseModel):
                    400: {"model": Message}
                }
                )
-async def teams_organisation(body: item):
+async def teams_organisation(body: teams_organisation_ids):
     """
     Delete a link between a team and an enclosure
     :param body: OBJECT (class) REQUIRED Contain Id of the enclosure and Id of the team
